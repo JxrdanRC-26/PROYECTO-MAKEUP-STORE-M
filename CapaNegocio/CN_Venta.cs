f7cbaad245b662680a072cb1f5bf9bf579bb8df1
JxrdanRC-26 <jxrdancc23@gmail.com>
2023-12-05 03:07:42 -0600

    PROYECTO MAKEUP STORE

---
+++ CapaNegocio/CN_Venta.cs
@@ -0,0 +1,100 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class CN_Venta
+    {
+        private CD_Venta objcd_venta = new CD_Venta();
+
+        public bool RestarStock(int idproducto, int cantidad)
+        {
+            return objcd_venta.RestarStock(idproducto, cantidad);
+        }
+
+        public bool SumarStock(int idproducto, int cantidad)
+        {
+            return objcd_venta.SumarStock(idproducto, cantidad);
+        }
+
+        public int ObtenerCorrelativo()
+        {
+            return objcd_venta.ObtenerCorrelativo();
+        }
+
+        public bool Registrar(Venta obj, DataTable DetalleVenta, out string Mensaje)
+        {
+            // Aplicar IVA en el monto total de cada detalle de venta
+            foreach (DataRow filaDetalle in DetalleVenta.Rows)
+            {
+                decimal precioVenta = Convert.ToDecimal(filaDetalle["PrecioVenta"]);
+                int cantidad = Convert.ToInt32(filaDetalle["Cantidad"]);
+
+                // Calcular el monto total con IVA
+                decimal montoTotalConIVA = CalcularMontoTotalItemConIVA(precioVenta, cantidad);
+                filaDetalle["SubTotal"] = montoTotalConIVA;
+            }
+
+            // Calcular el monto total con IVA para toda la venta
+            decimal montoTotalVentaConIVA = CalcularMontoTotalConIVA(DetalleVenta);
+
+            // Verificar si el monto pagado es suficiente
+            if (obj.MontoPago >= montoTotalVentaConIVA)
+            {
+                // Calcular el cambio correctamente
+                decimal cambio = obj.MontoPago - montoTotalVentaConIVA;
+
+                // Actualizar el monto total con IVA en el objeto Venta
+                obj.MontoTotal = montoTotalVentaConIVA;
+
+                // Actualizar el cambio en el objeto Venta
+                obj.MontoCambio = cambio;
+
+                // Luego, continuar con el resto del código de registro
+                return objcd_venta.Registrar(obj, DetalleVenta, out Mensaje);
+            }
+            else
+            {
+                // El monto pagado es insuficiente
+                Mensaje = "Monto pagado insuficiente";
+                return false;
+            }
+        }
+
+        private decimal CalcularMontoTotalItemConIVA(decimal precioVenta, int cantidad)
+        {
+            const decimal PorcentajeIVA = 0.16m; // 16% de IVA, ajusta según las leyes locales
+            decimal montoTotalItem = precioVenta * cantidad;
+            return montoTotalItem * (1 + PorcentajeIVA);
+        }
+
+        private decimal CalcularMontoTotalConIVA(DataTable DetalleVenta)
+        {
+            decimal montoTotalConIVA = 0;
+
+            foreach (DataRow filaDetalle in DetalleVenta.Rows)
+            {
+                decimal subTotal = Convert.ToDecimal(filaDetalle["SubTotal"]);
+                montoTotalConIVA += subTotal;
+            }
+
+            return montoTotalConIVA;
+        }
+
+        public Venta ObtenerVenta(string numero)
+        {
+            Venta oVenta = objcd_venta.ObtenerVenta(numero);
+
+            if (oVenta.IdVenta != 0)
+            {
+                List<Detalle_Venta> oDetalleVenta = objcd_venta.ObtenerDetalleVenta(oVenta.IdVenta);
+                oVenta.oDetalle_Venta = oDetalleVenta;
+            }
+
+            return oVenta;
+        }
+    }
+}
---
+++ CapaPresentacion/Login.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CapaPresentacion
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtdocumento = new System.Windows.Forms.TextBox();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btningresar = new FontAwesome.Sharp.IconButton();
+            this.btncancelar = new FontAwesome.Sharp.IconButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.logo_box = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 206);
+            this.label1.TabIndex = 0;
+            // 
+            // txtdocumento
+            // 
+            this.txtdocumento.Location = new System.Drawing.Point(260, 72);
+            this.txtdocumento.Name = "txtdocumento";
+            this.txtdocumento.Size = new System.Drawing.Size(222, 20);
+            this.txtdocumento.TabIndex = 3;
+            this.txtdocumento.TextChanged += new System.EventHandler(this.txtdocumento_TextChanged);
+            // 
+            // txtclave
+            // 
+            this.txtclave.Location = new System.Drawing.Point(260, 117);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.PasswordChar = '*';
+            this.txtclave.Size = new System.Drawing.Size(222, 20);
+            this.txtclave.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "No. de Documento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contraseña:";
+            // 
+            // btningresar
+            // 
+            this.btningresar.BackColor = System.Drawing.Color.Black;
+            this.btningresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btningresar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btningresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btningresar.ForeColor = System.Drawing.Color.White;
+            this.btningresar.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            this.btningresar.IconColor = System.Drawing.Color.White;
+            this.btningresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btningresar.IconSize = 21;
+            this.btningresar.Location = new System.Drawing.Point(263, 162);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(102, 32);
+            this.btningresar.TabIndex = 7;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btningresar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btningresar.UseVisualStyleBackColor = false;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Firebrick;
+            this.btncancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btncancelar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btncancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncancelar.ForeColor = System.Drawing.Color.White;
+            this.btncancelar.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
+            this.btncancelar.IconColor = System.Drawing.Color.White;
+            this.btncancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btncancelar.IconSize = 21;
+            this.btncancelar.Location = new System.Drawing.Point(380, 162);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(102, 32);
+            this.btncancelar.TabIndex = 8;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btncancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btncancelar.UseVisualStyleBackColor = false;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(257, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "INICIAR SESIÓN";
+            // 
+            // logo_box
+            // 
+            this.logo_box.BackColor = System.Drawing.Color.Transparent;
+            this.logo_box.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.logo_box.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.logo_box.Image = global::CapaPresentacion.Properties.Resources.makeup_store11;
+            this.logo_box.Location = new System.Drawing.Point(29, 17);
+            this.logo_box.Name = "logo_box";
+            this.logo_box.Size = new System.Drawing.Size(162, 140);
+            this.logo_box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo_box.TabIndex = 10;
+            this.logo_box.TabStop = false;
+            this.logo_box.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MAKEUP STORE";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(539, 206);
+            this.Controls.Add(this.logo_box);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtclave);
+            this.Controls.Add(this.txtdocumento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.logo_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtdocumento;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private FontAwesome.Sharp.IconButton btningresar;
+        private FontAwesome.Sharp.IconButton btncancelar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox logo_box;
+        private System.Windows.Forms.Label label2;
+    }
+}
